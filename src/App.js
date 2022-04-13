1365549842bcdcca4e4d9b7fc6bc744f5170a491
Matias <lodigiani.matias97@gmail.com>
2022-04-13 15:54:35 -0300

    Actualizacion carousel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 import DaysiNavBar from './components/DaysiNavBar';
 import FooterDaysi from './components/FooterDaysi';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 function App() {
-  return ( 
+  AOS.init();
+  return (
     <div className="App">
-      <DaysiNavBar/> 
-      <FooterDaysi/>
-  </div> 
+      <header>
+        <DaysiNavBar />
+      </header>
+      <body>
+        <div id='tabla-principal'>
+          <table id='renglon-tabla'>
+            <tr >
+              <td>
+                <div data-aos="fade-right">
+                  <h2>Estacion Natural MaThi es un almacen natural ubicado en el barrio de Flores donde podrás encontrar todo tipo de productos, entre ellos apto diabéticos, celíacos, hipertensos. Tambien hay frutos secos, semillas, especias, legumbres, cereales, etc. Te invitamos a conocer nuestro local en Avenida Directorio 2565. </h2>
+                </div>
+              </td>
+              <td id='celda-carousel'>
+                <div class="carousel w-full">
+                  <div id="item1" class="carousel-item w-full">
+                    <img src="https://i.ibb.co/MScrFvJ/Estacion1.jpg" alt="Estacion1" border="0" class="w-full" />
+                  </div>
+                  <div id="item2" class="carousel-item w-full">
+                    <img src="https://i.ibb.co/XJNdnSn/Estacion3.jpg" alt="Estacion3" border="0" class="w-full" />
+                  </div>
+                  <div id="item3" class="carousel-item w-full">
+                    <img src="https://i.ibb.co/8BHcPpZ/Estacion2.jpg" alt="Estacion2" border="0" class="w-full" />
+                  </div>
+                </div>
+                <div class="flex justify-center w-full py-2 gap-2">
+                  <a href="#item1" class="btn btn-xs">1</a>
+                  <a href="#item2" class="btn btn-xs">2</a>
+                  <a href="#item3" class="btn btn-xs">3</a>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </body>
+      <footer>
+        <FooterDaysi />
+      </footer>
+      
+    </div>
   );
 }
 
